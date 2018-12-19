978ddee757762f58a092a71a51f356d9493a29f6
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-19 17:09:24 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless user
     return head(:forbidden) unless @user.authentication(params[:password])
     session[user_id] = @user.id
   end
