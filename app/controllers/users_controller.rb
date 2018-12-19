6de397d9a422e58184948bf5926cdc778aed51d7
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-19 17:34:15 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
   end
 
@@ -13,8 +12,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
-
-
 end
