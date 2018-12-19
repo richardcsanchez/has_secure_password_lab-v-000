d063c519fd3ea831c515fbd01305c1ddb57a0d9f
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-19 16:45:20 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181219164021) do
+ActiveRecord::Schema.define(version: 20181219164430) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "user"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
